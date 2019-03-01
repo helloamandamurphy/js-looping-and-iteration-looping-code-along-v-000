544c2e64d81445b0ce7f7396c0961799541ae519
Amanda Murphy <helloamandamurphy@gmail.com>
2019-03-01 01:58:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function printBadges (names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
+  }
+  return names;
+}
+
+function tailsNeverFails() {
+  return Math.random() >=0.5;
+}
+
+while (tailsNeverFails()) {
+  console.log 
+}
